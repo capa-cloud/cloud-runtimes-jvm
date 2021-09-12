f40faa25b7874fcae9fcf2318ba553b64dda5649
kevinten10 <596823919@qq.com>
2021-09-12 22:55:44 +0800

    feat: init Metrics API

--- cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/domain/enhanced/MetricsRuntimes.java
+++ cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/domain/enhanced/MetricsRuntimes.java
@@ -1,10 +1,29 @@
 package group.rxcloud.cloudruntimes.domain.enhanced;
 
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
 /**
  * Metrics Runtimes standard API defined.
  */
 public interface MetricsRuntimes {
 
-    // TODO: 2021/8/31 API
     // see https://github.com/reactivegroup/cloud-runtimes-jvm/issues/2
+    /*
+     * transaction ： 跨越一段时间（请求）
+     * event ： 感兴趣的事件（异常）
+     * heartbeat : 周而复始发生的事件（GC）
+     * metric ： 多维度数据统计指标（业务）
+     */
+
+    Mono<Void> startTransaction(String name, String type, Map<String, String> values);
+
+    Mono<Void> endTransaction(String name, String type, Map<String, String> values);
+
+    Mono<Void> saveEvent(String name, String type, Map<String, String> values);
+
+    Mono<Void> saveHeartbeat(String name, String type, Map<String, String> values);
+
+    Mono<Void> saveMetric(String name, String type, Map<String, String> values);
 }
