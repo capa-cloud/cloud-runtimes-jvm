30d389be539eb919802aadbd08b129d84221d6a0
Xiong, Pin <pinxiongcn@foxmail.com>
2021-10-17 10:14:11 +0800

    feat: Supports checkstyle to check all source codes
    1. Add the rules to check source code
    2. Import maven-checkstyle-plugin
    3. Fix all unformatted source code
    
    see issue: https://github.com/reactivegroup/cloud-runtimes-jvm/issues/17

--- cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/client/DefaultCloudRuntimesClient.java
+++ cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/client/DefaultCloudRuntimesClient.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package group.rxcloud.cloudruntimes.client;
 
 import group.rxcloud.cloudruntimes.domain.core.binding.InvokeBindingRequest;
@@ -10,7 +26,14 @@ import group.rxcloud.cloudruntimes.domain.core.invocation.InvokeMethodRequest;
 import group.rxcloud.cloudruntimes.domain.core.pubsub.PublishEventRequest;
 import group.rxcloud.cloudruntimes.domain.core.secrets.GetBulkSecretRequest;
 import group.rxcloud.cloudruntimes.domain.core.secrets.GetSecretRequest;
-import group.rxcloud.cloudruntimes.domain.core.state.*;
+import group.rxcloud.cloudruntimes.domain.core.state.State;
+import group.rxcloud.cloudruntimes.domain.core.state.DeleteStateRequest;
+import group.rxcloud.cloudruntimes.domain.core.state.StateOptions;
+import group.rxcloud.cloudruntimes.domain.core.state.GetBulkStateRequest;
+import group.rxcloud.cloudruntimes.domain.core.state.TransactionalStateOperation;
+import group.rxcloud.cloudruntimes.domain.core.state.ExecuteStateTransactionRequest;
+import group.rxcloud.cloudruntimes.domain.core.state.SaveStateRequest;
+import group.rxcloud.cloudruntimes.domain.core.state.GetStateRequest;
 import group.rxcloud.cloudruntimes.utils.TypeRef;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
