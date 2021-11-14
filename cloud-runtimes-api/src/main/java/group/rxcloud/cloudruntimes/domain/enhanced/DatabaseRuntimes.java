75e624fe59dd46e6856adee7acc6989aa0b70c9e
kevinten10 <596823919@qq.com>
2021-11-14 21:07:53 +0800

    feat: add database runtimes api to enhanced refer to https://github.com/dapr/dapr/issues/3354
    + release: release 1.0.5.RELEASE version

--- cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/domain/enhanced/DatabaseRuntimes.java
+++ cloud-runtimes-api/src/main/java/group/rxcloud/cloudruntimes/domain/enhanced/DatabaseRuntimes.java
@@ -1,6 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package group.rxcloud.cloudruntimes.domain.enhanced;
 
-import group.rxcloud.cloudruntimes.domain.enhanced.database.*;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.CreateTableRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.CreateTableResponse;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.DeleteTableRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.DeleteTableResponse;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.GetConnectionRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.GetConnectionResponse;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.InsertRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.InsertResponse;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.QueryRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.QueryResponse;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.UpdateRequest;
+import group.rxcloud.cloudruntimes.domain.enhanced.database.UpdateResponse;
 import group.rxcloud.cloudruntimes.utils.TypeRef;
 import reactor.core.publisher.Mono;
 
